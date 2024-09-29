1378a42a50c15144c0a480946fd9a1997953d497
phuongtran31231027815 <phuongtran.31231027815@st.ueh.edu.vn>
2024-09-29 21:50:16 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace CS012_Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // string sinhvien1 = "A";
+            // string sinhvien2 = "B";
+            // string sinhvien3 = "C";
+
+            // Console.WriteLine(sinhvien1);
+            // Console.WriteLine(sinhvien2);
+            // Console.WriteLine(sinhvien3);
+
+            string[] ds;
+            ds = new string[3];
+
+            ds[0] = "A";
+            ds[1] = "B";
+            ds[2] = "C";
+
+            Console.WriteLine(ds[0]);
+            Console.WriteLine(ds[1]);
+            Console.WriteLine(ds[2]);
+               
+ 
+
+        }
+    }
+}
+

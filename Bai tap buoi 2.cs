c14a464faf057054b0ca2d20a2eedc9a4bca4d4d
phuongtran31231027815 <phuongtran.31231027815@st.ueh.edu.vn>
2024-09-30 17:58:07 +0700

    Add files via upload

---
+++ Bai tap buoi 2.cs
@@ -0,0 +1,114 @@
+﻿using System.Formats.Asn1;
+
+internal class Program
+{
+    private static void Main1(string[] args)
+    {
+        bai1();
+        bai2();
+        bai3();
+        bai4();
+        bai5();
+        bai6();
+        bai8();
+        bai9();
+        bai10();
+
+    }
+    static void bai1()
+    {
+        Console.Write("nhap vao so a: ");
+        int num_a1 = Convert.ToInt32(Console.ReadLine());
+        Console.Write("nhap vao so b: ");
+        int num_b1 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Tong hai so la: " + (num_a1 + num_b1));
+    }
+    static void bai2()
+    {
+        Console.Write("nhap vao so a: ");
+        int a2 = Convert.ToInt32(Console.ReadLine());
+        Console.Write("nhap vao so b: ");
+        int b2 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("truoc khi chuyen: a = {0}, b={1}", a2, b2);
+        a2 = a2 + b2;
+        b2 = a2 - b2;
+        a2 = a2 - b2;
+        Console.WriteLine("sau khi chuyen: a = {0}, b={1}", a2, b2);
+    }
+
+    static void bai3()
+    {
+        Console.Write("nhap vao so a: ");
+        double num_a3 = Convert.ToDouble(Console.ReadLine());
+        Console.Write("nhap vao so b: ");
+        double num_b3 = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Tich hai so la: " + (num_a3 * num_b3));
+
+    }
+    static void bai4()
+    {
+        Console.Write("nhap vao so feet: ");
+        double a4 = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine($"{a4} feet = {a4 * 0.3048} meter");
+    }
+    static void bai5()
+    {
+
+        Console.Write("Loai do ban muon chuyen sang la (C or F): ");
+        string a5 = Convert.ToString(Console.ReadLine());
+        a5 = a5.ToUpper();
+        if (a5 == "C")
+        {
+            Console.Write("Nhap vao so do F : ");
+            double b5 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("So do C la: " + ((b5 - 32) / 1.8));
+        }
+        else if (a5 == "F")
+        {
+            Console.Write("Nhap vao so do C : ");
+            double b5 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("So do F la: " + (1.8 * b5 + 32));
+        }
+        else
+        {
+            Console.WriteLine("Khong thoa man yeu cau");
+        }
+
+    }
+    static void bai6()
+    {
+        Console.WriteLine("Kich thuoc cua kieu bool la: " + sizeof(bool) + " bytes");
+        Console.WriteLine("Kich thuoc cua kieu byte la: " + sizeof(byte) + " bytes");
+        Console.WriteLine("Kich thuoc cua kieu char la: " + sizeof(char) + " bytes");
+        Console.WriteLine("Kich thuoc cua kieu decimal la: " + sizeof(decimal) + " bytes");
+        Console.WriteLine("Kich thuoc cua kieu double la: " + sizeof(double) + " bytes");
+        Console.WriteLine("Kich thuoc cua kieu float la: " + sizeof(float) + " bytes");
+        Console.WriteLine("Kich thuoc cua kieu int la: " + sizeof(int) + " bytes");
+        Console.WriteLine("Kich thuoc cua kieu long la: " + sizeof(long) + " bytes");
+        Console.WriteLine("Kich thuoc cua kieu sbyte la: " + sizeof(sbyte) + " bytes");
+        Console.WriteLine("Kich thuoc cua kieu short la: " + sizeof(short) + " bytes");
+        Console.WriteLine("Kich thuoc cua kieu unit la: " + sizeof(uint) + " bytes");
+        Console.WriteLine("Kich thuoc cua kieu ulong la: " + sizeof(ulong) + " bytes");
+        Console.WriteLine("Kich thuoc cua kieu ushort la: " + sizeof(ushort) + " bytes");
+
+    }
+    
+    static void bai8()
+    {
+        Console.Write("Nhap vao ban kinh r (cm): ");
+        double r = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Dien tich hinh tron ban kinh " + r + " cm la: " + (r * r * 3.14) + " cm^2");
+    }
+    static void bai9()
+    {
+        Console.Write("Nhap vao canh a9 (cm): ");
+        double a9 = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Dien tich hinh vuong canh " + a9 + " cm la: " + (a9 * a9) + " cm^2");
+    }
+    static void bai10()
+    {
+        Console.Write("Nhap vao so ngay can chuyen: ");
+        int day_a10 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine($" Ta co: {day_a10 / 365} nam va {(day_a10 % 365) / 7} tuan va {(day_a10 % 365) % 7} ngay ");
+    }
+}
---
+++ Bai tap buoi 5 -1.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Session2
+{
+    internal class Bai_tap_buoi_5
+    {
+        public static void Mainb(string[] args)
+        {
+            int[] a = new int[100];
+            InputRandomData(a);
+            BubbleSort(a);
+            PrintArray(a);
+            PrintArray(FindDupe(a));
+
+
+        }
+        static int[] BubbleSort(int[] a)
+        {
+            for (int i = 0; i < a.Length; i++)
+            {
+                for (int j = 0; j < a.Length - i - 1; j++)
+                {
+                    if (a[j] > a[j + 1])
+                    {
+                        int temp = a[j];
+                        a[j] = a[j + 1];
+                        a[j + 1] = temp;
+                    }
+                }
+            }
+            return a;
+        }
+        static void InputRandomData(int[] a)
+        {
+            Random r = new Random();
+            for (int i = 0; i < a.Length; i++)
+            {
+                a[i] = r.Next(0, 100);
+            }
+        }
+        static void PrintArray(int[] a)
+        {
+            foreach (int i in a)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+        }
+        static double CalculateAvg(int[] a)
+        {
+            double s = 0;
+            foreach (int i in a)
+            {
+                s += i;
+            }
+            return s / a.Length;
+        }
+        static bool CheckExist(int[] a, int val)
+        {
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (val == a[i])
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        static int FindIndex(int[] a, int val)
+        {
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (a[i] == val)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+        static int[] RemoveEle(ref int[] a, int ele)
+        {
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (a[i] == ele)
+                {
+                    for (int j = i; j < a.Length - 1; j++)
+                    {
+                        a[j] = a[j + 1];
+
+                    }
+                    i--;
+                    Array.Resize(ref a, a.Length - 1);
+                }
+
+            }
+
+            return a;
+        }
+        static void MaxMin(int[] a)
+        {
+            int min = a[0];
+            int max = a[0];
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (a[i] < min)
+                    min = a[i];
+                if (a[i] > max)
+                    max = a[i];
+            }
+            Console.WriteLine($"Max: {max}, Min: {min}");
+        }
+        static int[] Reserve(int[] a)
+        {
+
+            for (int i = 0; i < a.Length / 2; i++)
+            {
+                int temp = a[i];
+                a[i] = a[a.Length - i - 1];
+                a[a.Length - i - 1] = temp;
+            }
+            return a;
+        }
+        //static void BubbleSortString(char[] s)
+        //{
+        //    for (int i = 0; i < s.Length; i++)
+        //    {
+        //        for (int j = 0; j < s.Length - i - 1; j++)
+        //        {
+        //            if (char.ToLower(s[j]) > char.ToLower(s[j + 1]))
+        //            {
+        //                char temp = s[j];
+        //                s[j] = s[j + 1];
+        //                s[j + 1] = temp;
+        //            }
+        //        }
+        //    }
+        //}
+
+        static int[] FindDupe(int[] a)
+        {
+            int index = 0;
+            int[] c = new int[100];
+
+            bool[] CheckPrint = new bool[100];
+            for (int i = 0; i < a.Length; i++)
+            {
+                for (int j = i + 1; j < a.Length; j++)
+                {
+
+                    if (a[j] == a[i])
+                    {
+
+                        if (!CheckPrint[a[i]])
+                        {
+
+
+                            c[index] = a[i];
+                            index++;
+                            CheckPrint[a[i]] = true;
+                        }
+                        break;
+                    }
+                }
+            }
+            Array.Resize(ref c, index);
+            return c;
+        }
+        static int[] RemoveDupe(int[] a)
+        {
+            int[] c = FindDupe(a);
+            for (int i = 0; i < c.Length; i++)
+            {
+                RemoveEle(ref a, c[i]);
+            }
+            return a;
+        }
+
+    }
+}
